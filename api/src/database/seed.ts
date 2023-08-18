2ce60d7a211a1410fa861d2d465fd5fe7bdc2f6b
bruno.oliveira <brunotco4@gmail.com>
2023-08-18 14:33:07 +0100

    implemented seed for database

---
+++ api/src/database/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const users = [
+  {
+    username: 'admin',
+    email: 'admin@admin.com',
+    name: 'Administrator'
+  }
+]
+
+async function main() {
+  for (const user of users) {
+    await prisma.user.create({ data: user });
+  }
+}
+
+main()
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
