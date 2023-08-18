c3859dfabd2530f934ca17f028317e96b41da16e
bruno.oliveira <brunotco4@gmail.com>
2023-08-18 15:21:40 +0100

    implemented swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Chit-Chat')
+    .setDescription('Chit-Chat API description')
+    .setVersion('1.0-alpha')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
