e4ae1f52571f09909945a02ee77559fd16c53cab
bruno.oliveira <brunotco4@gmail.com>
2023-08-18 14:34:43 +0100

    changed api entrypoint

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): any {
-    return { data: 'API reachable' };
+  getHello(): string {
+    return 'Welcome to Chit-Chat API';
   }
 }
