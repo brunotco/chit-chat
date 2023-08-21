1a3fea2c7797ef8f418b31253eabd411905e243c
bruno.oliveira <brunotco4@gmail.com>
2023-08-21 20:38:45 +0100

    implemented jwt configuration

---
+++ api/src/modules/auth/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<any> {
+    const user = await this.authService.validateUser(payload);
+    if (!user) {
+      throw new HttpException('invalid_token', HttpStatus.UNAUTHORIZED);
+    }
+    return user;
+  }
+}
+
+export interface JwtPayload {
+  login: string;
+}
