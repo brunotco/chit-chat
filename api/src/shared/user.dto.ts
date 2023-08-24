536b0897d0df7558e0925cd4982faa6881983c67
bruno.oliveira <brunotco4@gmail.com>
2023-08-24 15:01:43 +0100

    refactored user dto classes

--- api/src/shared/user.dto.ts
+++ api/src/shared/user.dto.ts
@@ -1,43 +1,51 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { Role } from '@prisma/client';
-import { IsEmail, IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
 
-export class LoginUserDto {
-  @ApiProperty()
-  @IsNotEmpty()
-  login: string;
-  @ApiProperty()
-  @IsNotEmpty()
-  password: string;
-}
 export class CreateUserDto {
   @ApiProperty()
+  @IsString()
   @IsNotEmpty()
   username: string;
+
   @ApiProperty()
-  @IsNotEmpty()
   @IsEmail()
+  @IsNotEmpty()
   email: string;
+
   @ApiProperty()
+  @IsString()
   @IsNotEmpty()
   password: string;
+
   @ApiProperty()
+  @IsString()
   @IsNotEmpty()
   name: string;
-  @ApiPropertyOptional()
-  @IsOptional()
+
+  @ApiPropertyOptional({ enum: Role })
   @IsEnum(Role)
   role: Role;
 }
+export class LoginUserDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  login: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
 export class UpdateUserPasswordDto {
   @ApiProperty()
+  @IsString()
   @IsNotEmpty()
   old_password: string;
+
   @ApiProperty()
+  @IsString()
   @IsNotEmpty()
   new_password: string;
 }
-// export class SimpleUserDto {
-// }
-// export class FullUserDto extends SimpleUserDto {
-// }
