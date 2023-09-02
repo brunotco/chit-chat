2b6e48361fa8363b8a33461ce8037beccbfdb94d
brunotco <brunotco4@gmail.com>
2023-09-02 01:35:11 +0100

    added types

--- client/src/app/api/api.service.ts
+++ client/src/app/api/api.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginForm } from '../models/login-form.model';
+import { LoginResponse } from '../models/login-response.model';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
-  login(login: any) {
-    return this.httpClient.post('/api/auth/login', login);
+  login(login: LoginForm): Observable<LoginResponse> {
+    return this.httpClient.post<LoginResponse>('/api/auth/login', login);
   }
 
   getUsers() {
