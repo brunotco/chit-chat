0b741ee58d9d7eb4d04114884e9fe96614ddee9c
bruno.oliveira <brunotco4@gmail.com>
2023-09-06 09:56:17 +0100

    enabling cors for deploy testing

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Chit-Chat')
     .setDescription('Chit-Chat API description')
